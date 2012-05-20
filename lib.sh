c2341dc6e742e36d4611c6e0de6fdae5c0465942
Pepijn Verlaan <pepijnverlaan@yahoo.com>
2012-05-20 15:28:15 +0300

    improve pretty printing and rounding logic (and spec)

--- lib.sh
+++ lib.sh
@@ -6,51 +6,62 @@ STATE_WARNING=1
 STATE_CRITICAL=2
 STATE_UNKNOWN=3
 
-# Convert '2456' to '2.5 kB' and '133000' to '133 kB'
+# Convert '2456' to '2.45 kB' and '134000' to '133 kB'
 function bytes_to_human() {
-	UNITS=(
-		"PB $((1000 * 1000 * 1000 * 1000 * 1000))"
-		"TB $((1000 * 1000 * 1000 * 1000))"
-		"GB $((1000 * 1000 * 1000))"
-		"MB $((1000 * 1000))"
-		"kB $((1000))"
-	)
-
-	bytes_to_human_impl $@
+	bytes_to_human_impl "B kB MB GB TB PB" 1000 $@
 }
 
-# Convert '2456' to '2.4 kB' and '133000' to '130 kB'
+# Convert '2456' to '2.39 KiB' and '134000' to '130 KiB'
 function bytes_to_human_bi() {
-	UNITS=(
-		"PiB $((1024 * 1024 * 1024 * 1024 * 1024))"
-		"TiB $((1024 * 1024 * 1024 * 1024))"
-		"GiB $((1024 * 1024 * 1024))"
-		"MiB $((1024 * 1024))"
-		"KiB $((1024))"
-	)
-
-	bytes_to_human_impl $@
+	bytes_to_human_impl "B KiB MiB GiB TiB PiB" 1024 $@
 }
 
-# Only call this after you've set the UNITS variable
-# This function only supports Bash integers (64 bit unsigned on my system)
 function bytes_to_human_impl() {
-	for UNIT in "${UNITS[@]}"; do
-		UNIT=($UNIT)
-		SYMBOL=${UNIT[0]}
-		NUM=${UNIT[1]}
+	UNITS=($1)
+	BASE=$2
+	BYTES=$3
 
-		if [ $1 -ge $NUM ]; then
-			bc -l <<< "
-				result = $1 / $NUM
-				result_ln = l(result);
-				ten_ln = l(10);
-				scale = 0;
-				scale = 2 - result_ln / ten_ln;
-				print result / 1, \" $SYMBOL\n\"" 2>/dev/null
-			return
-		fi
-	done
+	# This script works for pretty large non-negative integers (tested up to 100
+	# digits), and it truncates to at least three significant digits.
+	# 'Normal' rounding would need to take into account that 1048064 should be
+	# displayed as 1.00 MiB, which it won't if you calculate the unit before the
+	# rounding takes place (it will display 1024 KiB). Somehow the calculation
+	# of the unit would have to incorporate the rounding, and I can't come up
+	# with the math to do that.
+	BC=($(bc -l <<< "
+		/* Special-case everything less than the first unit */
+		if ($BYTES < $BASE) {
+			$BYTES
+			0
+			halt
+		}
 
-	echo $1 B
+		/* First compute and round the unit to use */
+		scale = length($BYTES) + 2
+		unit = l($BYTES) / l($BASE)
+		scale = 0
+		unit /= 1
+
+		/* We do not have all units available */
+		if (unit >= ${#UNITS[@]}) {
+			unit = ${#UNITS[@]} - 1
+		}
+
+		/* Compute the result for the unit */
+		scale = 3
+		result = $BYTES / $BASE ^ unit
+
+		/* Set the number of digits to show after the decimal point */
+		if (length(result) > 6) {
+			scale = 0
+		} else {
+			scale = 6 - length(result)
+		}
+
+		/* Print the rounded result and its unit */
+		result / 1
+		unit
+		"))
+
+	echo ${BC[0]} ${UNITS[${BC[1]}]}
 }
